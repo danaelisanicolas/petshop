4f2e30221ba6e1383906e1a9e0508d303f302383
Dana Elisa Nicolas <danaelisa.nicolas@gmail.com>
2020-11-18 18:19:28 +1100

    exploring css modules

---
+++ petshop/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { Navigation } from "../navigation/navigation"
+
+export default function Home() {
+  return (
+    <Navigation />
+  )
+}
